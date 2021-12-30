64e40e9eb68096dcd03f4d9447b7caa5635a8696
DESKTOP-F6TLTDN\hk416 <syoei-hanashiro-20f@stu.kbc.ac.jp>
2021-12-30 17:01:23 +0900

    仮ステージと仮追跡

---
+++ ora-ora-ora-orangoutan/Assets/Scripts/tuiseki.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class tuiseki : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+    private Transform enemy;
+    bool hit = false;
+    float downtime = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        enemy = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (hit == false)
+        {
+            agent.destination = target.position;
+        }
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            hit = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            hit = false;
+        }
+    }
+}
