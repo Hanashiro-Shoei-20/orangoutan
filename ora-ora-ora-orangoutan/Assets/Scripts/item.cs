d5efaabaf6788be2fee0ac01a42e60ac91f4029b
KBC12005021\kbc12005021 <taichi-tomoyose-20f@stu.kbc.ac.jp>
2022-01-04 17:33:13 +0900

    taba

---
+++ ora-ora-ora-orangoutan/Assets/Scripts/item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    void OnTriggerEnter(Collision other)
+	{
+		// 衝突した相手にPlayerタグが付いているとき
+		if (other.gameObject.tag == "Player")
+		{
+			Debug.Log("当たった");
+			// 0.2秒後に消える
+			Destroy(gameObject, 0.2f);
+		}
+	}
+}
